f500cdf198a80b0835eb75b49ebf8a6be7802c49
Martin Terskin <martinas.terskin@gmail.com>
2016-04-28 11:41:18 -0400

    project presentation commit #1

--- db/migrate/20160427195206_create_join_table_place_user.rb
+++ db/migrate/20160427195206_create_join_table_place_user.rb
@@ -1,5 +1,5 @@
-class CreateJoinTablePlacetypeUser < ActiveRecord::Migration
-  def change
+class CreateJoinTablePlaceUser < ActiveRecord::Migration
+  def changes
     create_join_table :placetypes, :users do |t|
       # t.index [:placetype_id, :user_id]
       # t.index [:user_id, :placetype_id]
